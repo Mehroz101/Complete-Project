9a0ad0ad5e2b15ad80c06147dd8dcfdf8ffb71a8
Mehroz101 <mehrozfarooq127@gmail.com>
2024-12-07 17:32:45 +0500

    creating about us page

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,34 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/AboutUs.css'; // Make sure to create this CSS file
-import Navbar from '../components/Navbar';
-import AboutusImg from "../assets/aboutus.jpg"
-import Footer from '../components/Footer';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/AboutUs.css"; // Make sure to create this CSS file
+import Navbar from "../components/Navbar";
+import AboutusImg from "../assets/aboutus.jpg";
+import AboutusTopImg from "../assets/aboutustop.png";
+import Footer from "../components/Footer";
 
 const AboutUs = () => {
   return (
     <>
-    <Navbar/>
-    <div className="about_us_section">
-      <div className="about_us_image">
-        <img 
-          src={AboutusImg}
-          alt="About Us Illustration"
-        />
+      <Navbar />
+      <div className="about_us_section">
+        <div className="aboutus_top">
+          <div className="about_us_image">
+            <img src={AboutusTopImg} alt="About Us Illustration" />
+          </div>
+          <div className="top_text">
+            <h1>About Us</h1>
+          </div>
+        </div>
+        <div className="about_us">
+          <div className="text">
+            <h1>Passionate Minds Crafting Digital Experiences</h1>
+            <p>
+              As a team, we believe in the power of innovation and technology to
+              transform the way businesses connect with their audience. Our
+              journey began with a simple idea: to create digital solutions that
+              are as unique as the people behind them. Today, we stand as a team
+              of creative thinkers, developers, and designers united by a shared
+              passion for excellence.
+            </p>
+          </div>
+
+          <img src={AboutusImg} alt="" />
+        </div>
       </div>
-      <div className="about_us_content">
-        <h2>About Us</h2>
-        <p>
-          We are dedicated to providing the best parking solutions for our customers.
-          With a focus on innovation and user satisfaction, we ensure that finding 
-          and reserving parking spaces is as seamless as possible.
-        </p>
-        <Link to="/contact" className="contact_us_button">
-          Contact Us
-        </Link>
-      </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
